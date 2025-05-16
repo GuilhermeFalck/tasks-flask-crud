f23da6f8f5b43ce3f7d8c6983f8167bf948bee8d
GuilhermeFalck <guilhermefalck22@gmail.com>
2025-05-16 10:08:09 -0300

    add app.py and requirements.txt

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/about")
+def about():
+    return "Página sobre"
+
+if __name__ == "__main__":
+    app.run(debug=True) 
